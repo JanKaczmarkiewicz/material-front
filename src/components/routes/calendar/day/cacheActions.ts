4937873d13c1e9dd703f1ad9ceb7752a9d0adadf
jan00newbie <positiveKiddo@gmail.com>
2020-08-07 17:10:13 +0200

    override cache per street removed

--- src/components/routes/calendar/day/cacheActions.ts
+++ src/components/routes/calendar/day/cacheActions.ts
@@ -1,38 +1,56 @@
-import { ChangeAssignedStreets_updateDay } from "../../../../generated/ChangeAssignedStreets";
 import {
-  DayVariables,
-  Day,
-  Day_day,
   Day_day_pastoralVisits_entrances_house,
-  Day_day_assignedStreets_unusedHouses,
   Day_day_assignedStreets,
+  Day,
+  DayVariables,
 } from "../../../../generated/Day";
 import { client } from "../../../../context/client/ApolloClient";
-import { DAY, HouseFragment } from "../actions";
-import { getKeys } from "../../../Layout/DataTable/util";
+import { DAY } from "../actions";
 import { gql } from "apollo-boost";
 import produce from "immer";
-import { StreetFragment } from "../../data/streets/actions";
-
-type HousesGroupedByStreetId = {
-  [key: string]: Day_day_pastoralVisits_entrances_house[];
-};
+import { splitByLabelWithoutIndex } from "../../../../utils/splitByLabel";
+import { ChangeAssignedStreets_updateDay } from "../../../../generated/ChangeAssignedStreets";
 
 export const updateStreets = (
-  houses: HousesGroupedByStreetId,
-  seasonId: string
+  removedStreetsIds: string[],
+  dayVariables: DayVariables
 ) => {
-  for (const streetId of Object.keys(houses)) {
-    const fragment = gql`
+  const dayQuery = client.readQuery<Day, DayVariables>({
+    query: DAY,
+    variables: dayVariables,
+  });
+
+  if (!dayQuery || !dayQuery.day) return;
+
+  const removedHouses = dayQuery.day.pastoralVisits.flatMap(
+    ({ entrances }) =>
+      entrances
+        .filter(({ house }) =>
+          removedStreetsIds.includes(house?.street?.id || "")
+        )
+        .map(({ house }) => house) as Day_day_pastoralVisits_entrances_house[]
+  );
+
+  const splitedRemovedHouses = splitByLabelWithoutIndex(
+    removedHouses,
+    (house) => house?.street?.id
+  );
+
+  const fragment = gql`
     fragment AssignedStreetFragment on Street {
-      ...StreetFragment
-      unusedHouses(season: ${seasonId}){
-        ...HouseFragment
+      name
+      id
+      unusedHouses(season: "${dayVariables.season}"){
+        number
+        street {
+          name
+          id
+        }
       }
     }
-    ${StreetFragment}
-    ${HouseFragment}`;
+    `;
 
+  for (const streetId of Object.keys(splitedRemovedHouses)) {
     const streetQuery = client.readFragment<Day_day_assignedStreets>({
       id: streetId,
       fragment,
@@ -41,7 +59,7 @@ export const updateStreets = (
     if (!streetQuery) continue;
 
     const updatedStreet = produce(streetQuery, (draft) => {
-      draft.unusedHouses.push(...houses[streetId]);
+      draft.unusedHouses.push(...splitedRemovedHouses[streetId]);
     });
 
     client.writeFragment<Day_day_assignedStreets>({
@@ -50,4 +68,20 @@ export const updateStreets = (
       data: updatedStreet,
     });
   }
+};
+
+export const assignDayStateAfterAssignedStreetsChanged = (
+  dayVariables: DayVariables,
+  day: ChangeAssignedStreets_updateDay
+) => {
+  const query = client.readQuery<Day, DayVariables>({
+    query: DAY,
+    variables: dayVariables,
+  });
+  if (!query || !query.day) return;
+  client.writeQuery<Day, DayVariables>({
+    query: DAY,
+    variables: dayVariables,
+    data: { day: { ...query.day, ...day } },
+  });
 };
